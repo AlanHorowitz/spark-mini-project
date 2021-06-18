84c64fe2135259f137173d1f6af4fc60777ec646
AlanHorowitz <alanhorowitz63@gmail.com>
2021-06-18 12:22:57 -0400

    local pyspark version

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from pyspark import SparkContext
+from operator import add
 
 
 def extract_vin_key_value(line):
@@ -9,21 +10,29 @@ def extract_vin_key_value(line):
 
 
 def populate_make(values):
-    """expect iterable of values"""
-
     make, year = '', ''
     for value in values:
-        print(value)
         if value[0] == 'I':
             make, year = value[1], value[2]
             break
     return [['A', make, year] for value in values if value[0] == 'A']
 
 
-sc = SparkContext('local','spark-mini-project')
+def extract_make_key_value(x):
+    return x[1]+'-'+x[2], 1
+
+
+def to_csv_format(x):
+    return x[0] + ',' + str(x[1])
+
+
+sc = SparkContext('local', 'spark-mini-project')
 raw_rdd = sc.textFile('data.csv')
 
 vin_kv = raw_rdd.map(lambda x: extract_vin_key_value(x))
-print(type(vin_kv))
-o = vin_kv.groupByKey().flatMap(lambda kv: populate_make(kv[1]))
-print(o.collect())
+enhance_make = vin_kv.groupByKey().flatMap(lambda kv: populate_make(kv[1]))
+make_kv = enhance_make.map(lambda x: extract_make_key_value(x))
+accident_counts = make_kv.reduceByKey(add).map(to_csv_format)
+accident_counts.saveAsTextFile("out.txt")
+
+
